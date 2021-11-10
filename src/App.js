a55e201b9f921284aadb089b6b17e609cbc83fea
Anushka7310 <61081130+Anushka7310@users.noreply.github.com>
2021-11-10 15:00:10 +0530

    added function to show input messages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,39 @@
+import  React, { useState } from 'react';
 import './App.css';
+import { Button } from '@material-ui/core';
 
 function App() {
+  const [input, setInput] = useState(''); 
+  const [messages, setMessages] = useState([]);
+
+  console.log(input);
+  console.log(messages);
+  
+  const sendMessage = (event) => {
+    event.preventDefault();
+    // aal th elogic to send a message goes
+    setMessages([...messages, input])
+    setInput('');
+
+  }
+
+
   return (
     <div className="App">
-      Hello World I am Anushka pyari si 
+      <h1>Hello </h1>
+
+    <form>
+      <input value={input} onChange={event => setInput(event.target.value)} />
+      <Button  type='submit' onClick={sendMessage}> Send Message</Button>
+    </form>
+    
+    {/*messages themselves*/}
+  
+  {
+  messages.map(messages => (
+    <p>{messages}</p>
+  ))
+  }
     </div>
   );
 }
