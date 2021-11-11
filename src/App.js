8feee2d5094aafe14732b7b3c70cf873cd8dc244
Anushka7310 <61081130+Anushka7310@users.noreply.github.com>
2021-11-11 22:24:48 +0530

    added form control and message component

--- src/App.js
+++ src/App.js
@@ -1,37 +1,39 @@
 import  React, { useState } from 'react';
 import './App.css';
-import { Button } from '@material-ui/core';
+import { Button, FormControl, InputLabel, Input } from '@material-ui/core';
+import Message from './Message';
 
 function App() {
   const [input, setInput] = useState(''); 
-  const [messages, setMessages] = useState([]);
+  const [messages, setMessages] = useState(['']);
 
   console.log(input);
   console.log(messages);
   
   const sendMessage = (event) => {
     event.preventDefault();
-    // aal th elogic to send a message goes
-    setMessages([...messages, input])
+    
+    setMessages([...messages, input]);
     setInput('');
-
   }
 
 
   return (
     <div className="App">
       <h1>Hello </h1>
-
-    <form>
-      <input value={input} onChange={event => setInput(event.target.value)} />
-      <Button  type='submit' onClick={sendMessage}> Send Message</Button>
-    </form>
     
-    {/*messages themselves*/}
+      <form>
+      <FormControl>
+        <InputLabel>Enter a message...</InputLabel>
+        <Input value={input} onChange={event => setInput(event.target.value)} />
+        <Button diasabled={!input} variant="contained" color="primary" type='submit' onClick={sendMessage}> Send Message</Button>
+      </FormControl>
+      </form>
   
   {
-  messages.map(messages => (
-    <p>{messages}</p>
+   messages.map(message => (
+    <Message text={message} />
+   
   ))
   }
     </div>
