b0a27449a31652f11f2a92d6d98151489950545d
Anushka7310 <61081130+Anushka7310@users.noreply.github.com>
2021-11-12 23:08:47 +0530

    updated message styles and material-ui

--- src/Message.js
+++ src/Message.js
@@ -2,17 +2,17 @@ import React from 'react'
 import { Card, CardContent, Typography } from "@material-ui/core";
 import './Message.css';
 
-function Message(message, username) {
+function Message({message, username}) {
     const isUser = username === message.username;
 
     return (
-        <div>
+        <div className={ `message ${isUser && 'message_user'}`}>
         <Card className={isUser ? "message_userCard" : "message_guestCard" }>
             <CardContent>
                 <Typography
-                color="white"
-                variant="h5"
-                component="h2"
+                 color="white"
+                 variant="h5"
+                 component="h2"
             >
                 {message.username}: {message.text}
             </Typography>
